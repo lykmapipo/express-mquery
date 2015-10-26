79e1cbd72ebec2479ead7a876f251d1d0ba3259f
lykmapipo <lallyelias87@gmail.com>
2015-10-26 10:24:25 +0300

    add mquery mongoose plugin

--- index.js
+++ index.js
@@ -2,11 +2,27 @@
 
 //dependencies
 var path = require('path');
-var parser = require(path.join(__dirname, 'lib', 'parser'));
-var middleware = require(path.join(__dirname, 'lib', 'middleware'));
+var buildQuery = require(path.join(__dirname, 'lib', 'buildQuery'));
+var prepareQuery = require(path.join(__dirname, 'lib', 'prepareQuery'));
 
 //export express middleware
-exports.middleware = middleware;
+exports.middleware = prepareQuery;
 
 //export mongoose plugin
-exports.parser = parser;
+exports.mquery = function(schema, options) {
+    options = options || {};
+
+    /**
+     * @description build mongoose query from request.mquery object
+     * @param  {Request} request valid express HTTP request
+     * @return {Query}         valid mongoose query instance
+     */
+    schema.statics.mquery = function(request) {
+        var query = options.query || this.find();
+        if (request.mquery) {
+            return buildQuery(options)(query, request.mquery);
+        } else {
+            return query;
+        }
+    };
+};
