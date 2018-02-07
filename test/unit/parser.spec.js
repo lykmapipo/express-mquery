a5ff26bd45e93ad12d99bac29d329eb985374ddf
lykmapipo <lallyelias87@gmail.com>
2018-02-07 13:43:44 +0300

    add json api filters style with search with q enabled

--- test/unit/parser.spec.js
+++ test/unit/parser.spec.js
@@ -800,4 +800,48 @@ describe.only('parse', function() {
   });
 
 
+  describe('filters', function() {
+
+    it('should be a function', function() {
+      expect(parser.filter).to.exist;
+      expect(parser.filter).to.be.a('function');
+    });
+
+
+    describe('search', function() {
+
+      it('should parse search query', function(done) {
+        const query = { filters: { q: 'a' } };
+
+        parser
+          .filter(JSON.stringify(query), function(error, filter) {
+            expect(error).to.not.exist;
+            expect(filter).to.exist;
+            expect(filter.q).to.exist;
+            expect(filter.q).to.be.eql(query.filters.q);
+            done(error, filter);
+          });
+
+      });
+
+      it('should parse search query', function(done) {
+
+        const query = { q: 'a' };
+
+        parser
+          .filter(JSON.stringify(query), function(error, filter) {
+            expect(error).to.not.exist;
+            expect(filter).to.exist;
+            expect(filter.q).to.exist;
+            expect(filter.q).to.be.eql(query.q);
+            done(error, filter);
+          });
+
+      });
+
+    });
+
+  });
+
+
 });
